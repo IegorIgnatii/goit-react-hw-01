de7301b59bcf0a1a18796666473728a683a32cb5
IegorIgnatii <165487943+IegorIgnatii@users.noreply.github.com>
2024-08-05 17:42:16 +0300

    go

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,27 @@
 import "./App.css";
-// import Profile from "./components/Profile/Profile";
-// import userData from "./components/userData.json";
+
+import userData from "./components/userData.json";
+
 import friends from "./components/friends.json";
+
+import transactions from "./components/transactions.json";
+
+import Profile from "./components/Profile/Profile";
 import FriendList from "./components/FriendList/FriendList";
-// import transactions from "./components/transactions.json";
-// import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
+import TransactionHistory from "./components/TransactionHistory/TransactionHistory";
+
 const App = () => {
   return (
     <>
-      {/* <Profile
+      <Profile
         name={userData.username}
         tag={userData.tag}
         location={userData.location}
         image={userData.avatar}
         stats={userData.stats}
-      /> */}
+      />
       <FriendList friends={friends} />
-      {/* <TransactionHistory items={transactions} /> */}
+      <TransactionHistory items={transactions} />
     </>
   );
 };
